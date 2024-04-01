1a389753a632156a5574c65b607b1e80cb4a5efb
GARVIT BANSAL 292712 <118173085+GarvitBansal292712@users.noreply.github.com>
2024-04-01 22:47:13 +0530

    Try 1 for Fixing the Shipping Component Error

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Faq from "./components/Faq";
-import ShippingReturn from "./components/ShippingReturn";
 import StorePolicy from "./components/StorePolicy";
 import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import ProductPage from "./components/ProductPage";
-
+import ShippingReturn from "./components/ShippingReturn";
 function App() {
   return (
     <>
